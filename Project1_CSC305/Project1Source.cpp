39b7d834b5ae483a497f4f4efe8fcd72ca85e21c
Micaiah Skolnick <micaiahskolnickdeveloper@gmail.com>
2017-09-03 01:42:40 -0500

    2nd commit

--- Project1_CSC305/Project1Source.cpp
+++ Project1_CSC305/Project1Source.cpp
@@ -16,16 +16,49 @@ void write(string text) {
 	toFile.close();
 
 }
+void addDialog() {
+	string code, cCode, cName;
+
+	cout << "Add dialog entered:" << endl;
+	cout << "Type 'c' to add  city, 't' to add a team, or 'g' to add a game";
+	cin >> code;
+	if (code == "c") {
+		cin >> cCode;
+		cin >> cName;
+		write("<city><" + cCode + ">" + "<" + cName + ">");
+	}
+	else if (code == "g") {
+
+	}
+	else if (code == "t") {
+
+	}
+
+}
+void listDialog() {
+
+}
 
 int main() {
 
 
-	string instruction;
-	//Read instructions to perform on game listing
-	while (instruction != "x") {
-		cin >> instruction;
-		write(instruction);
-
+	string code,cCode,cName;
+	//Read instructions to perform until an instruction to quit has been reached
+	while (code != "q") {
+		cout << "Please tell me what to do by typing one of the following codes:" << endl;
+		cout << "'a' to add data" << endl;
+		cout << "'l' to return a list of data" << endl;
+		cout << "'r' for results of a specific team" << endl;
+		cout << "'s' for standings of all the teams" << endl;
+		cout << "'q' to quit" << endl;
+		cin >> code;
+			//For a code of "a" enter the add dialog
+		if (code == "a") {
+			addDialog();			
+		}
+		else if (code == "l") {
+			listDialog();
+		}
 	}
 
 	return 0;
